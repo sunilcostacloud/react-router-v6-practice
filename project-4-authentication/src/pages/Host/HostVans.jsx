1288f952385ed75b5cd5a627a4063d369bb3391e
sunilcostacloud <sunil@costacloud.com>
2023-09-27 11:06:19 +0530

    update

---
+++ project-4-authentication/src/pages/Host/HostVans.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const HostVans = () => {
+    const [vans, setVans] = useState([]);
+
+    useEffect(() => {
+        fetch("/api/host/vans")
+            .then((res) => res.json())
+            .then((data) => setVans(data.vans));
+    }, []);
+
+    const hostVansEls = vans.map((van) => (
+        <Link to={van.id} key={van.id} className="host-van-link-wrapper">
+            <div className="host-van-single" key={van.id}>
+                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                <div className="host-van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                </div>
+            </div>
+        </Link>
+    ));
+
+    return (
+        <div>
+            <section>
+                <h1 className="host-vans-title">Your listed vans</h1>
+                {vans.length > 0 ? (
+                    <div className="host-vans-list">
+                        <section>{hostVansEls}</section>
+                    </div>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </section>
+        </div>
+    );
+};
+
+export default HostVans;
